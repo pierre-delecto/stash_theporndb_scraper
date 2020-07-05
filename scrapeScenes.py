a5a2ef4ebb3435e92544037c8034748c56c57846
pierre-delecto <fakeinbox@gmail.com>
2020-07-05 14:55:10 -0700

    Fix potential error when TPDB returns invalid data

--- scrapeScenes.py
+++ scrapeScenes.py
@@ -704,7 +704,7 @@ def getPerformer(name):
     try:
         result = requests.get(search_url).json()
         tpbd_error_count = 0
-        if result.get("data", [{}])[0].get("id", None):
+        if  next(iter(result.get("data", [{}])), [{}]).get("id", None):
             performer_id = result["data"][0]["id"]
             return requests.get(data_url_prefix+performer_id).json()["data"]
         else:
