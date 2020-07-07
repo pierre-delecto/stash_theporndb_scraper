e55d79d48d6b92216b13841d3a25d6c1014076ac
pierre-delecto <fakeinbox@gmail.com>
2020-07-07 14:05:35 -0700

    Various updates

--- configuration.py
+++ configuration.py
@@ -4,12 +4,15 @@ server_ip= "<IP_ADDRESS>"  #Don't include the '<' or '>'
 server_port = "<PORT>" #Don't include the '<' or '>'
 username="<USERNAME>" #Don't include the '<' or '>'
 password="<PASSWORD>" #Don't include the '<' or '>'
+ignore_ssl_warnings= True # Set to True if your Stash uses SSL w/ a self-signed cert
 
 # Configuration options
-scrape_tag= "scraped_from_theporndb"  #Tag to be added to scraped scenes.  Set to None to disable
+scrape_tag= "Scraped From ThePornDB"  #Tag to be added to scraped scenes.  Set to None to disable
+unmatched_tag = "Missing From ThePornDB" #Tag to be added to scenes that aren't matched at TPDB.  Set to None to disable.
 disambiguate_only = False # Set to True to run script only on scenes tagged due to ambiguous scraping. Useful for doing manual disambgiuation.  Must set ambiguous_tag for this to work
 verify_aliases_only = False # Set to True to scrape only scenes that were skipped due to unconfirmed aliases - set confirm_questionable_aliases to True before using
-rescrape_scenes= True # If False, script will not rescrape scenes previously scraped successfully.  Must set scrape_tag for this to work
+rescrape_scenes= False # If False, script will not rescrape scenes previously scraped successfully.  Must set scrape_tag for this to work
+retry_unmatched = False # If False, script will not rescrape scenes previously unmatched.  Must set unmatched_tag for this to work
 debug_mode = False
 
 #Set what fields we scrape
@@ -31,7 +34,7 @@ add_performers = True
 #The script tries to disambiguate using title, studio, and date (or just filename if parse_with_filename is True).  If this combo still returns more than one result, these options are used.  Set both to False to skip scenes with ambiguous results
 auto_disambiguate = False  #Set to True to try to pick the top result from ThePornDB automatically.  Will not set ambiguous_tag
 manual_disambiguate = False #Set to True to prompt for a selection.  (Overwritten by auto_disambiguate)
-ambiguous_tag = "theporndb_ambiguous" #Tag to be added to scenes we skip due to ambiguous scraping.  Set to None to disable
+ambiguous_tag = "ThePornDB Ambiguous" #Tag to be added to scenes we skip due to ambiguous scraping.  Set to None to disable
 #Disambiguation options for when a specific performer can't be verified
 tag_ambiguous_performers = True  # If True, will tag ambiguous performers (performers listed on ThePornDB only for a single site, not across sites)
 confirm_questionable_aliases = True #If True, when TPBD lists an alias that we can't verify, manually prompt for config.  Otherwise they are tagged for later reprocessing
@@ -46,5 +49,4 @@ get_images_babepedia = True #If True, will try to grab an image from babepedia b
 include_performers_in_title = True #If True, performers will be added at the beggining of the title
 clean_filename = True #If True, will try to clean up filenames before attempting scrape. Often unnecessary, as ThePornDB already does this
 compact_studio_names = True # If True, this will remove spaces from studio names added from ThePornDB
-ignore_ssl_warnings= True # Set to True if your Stash uses SSL w/ a self-signed cert
 proxies={} # Leave empty or specify proxy like this: {'http':'http://user:pass@10.10.10.10:8000','https':'https://user:pass@10.10.10.10:8000'}
