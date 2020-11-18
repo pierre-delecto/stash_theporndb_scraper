994afbe8b99c3085f33a236c3572bb504aabda56
pierre-delecto <60908642+pierre-delecto@users.noreply.github.com>
2020-11-18 14:29:58 -0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-result = {"data":[{"what":"1"}]}
-
-if  next(iter(result.get("data", [{}])), {}).get("id", None):
-    performer_id = result["data"][0]["id"]
-    print(result["data"][0]["id"])
-else:
-    print("No")
